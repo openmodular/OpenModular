49e79a37d67ffa2cc86ee0fe29565cce0221a278
OLDLI <wenju1991@163.com>
2024-10-20 23:12:38 +0800

    feature:完善Check类

--- src/Commonn/Common.Utils/Check.cs
+++ src/Commonn/Common.Utils/Check.cs
@@ -50,4 +50,76 @@ public static class Check
 
         return value;
     }
+
+    /// <summary>
+    /// 确保给定的集合不为null或者空集合
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="collection"></param>
+    /// <param name="parameterName"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static IEnumerable<T> NotNullOrEmpty<T>(IEnumerable<T>? collection, string parameterName)
+    {
+        if (collection == null || !collection.Any())
+        {
+            throw new ArgumentException($"{parameterName} can not be null or empty!", parameterName);
+        }
+
+        return collection;
+    }
+
+    /// <summary>
+    /// 确保前值小于后值
+    /// </summary>
+    /// <exception cref="ArgumentException"></exception>
+    public static int LessThan(int value, int maxValue, string parameterName)
+    {
+        if (value >= maxValue)
+        {
+            throw new ArgumentException($"{parameterName} must be less than {maxValue}!", parameterName);
+        }
+
+        return value;
+    }
+
+    /// <summary>
+    /// 确保前值小于等于后值
+    /// </summary>
+    /// <exception cref="ArgumentException"></exception>
+    public static int LessThanOrEqual(int value, int maxValue, string parameterName)
+    {
+        if (value > maxValue)
+        {
+            throw new ArgumentException($"{parameterName} must be less than or equal to {maxValue}!", parameterName);
+        }
+
+        return value;
+    }
+
+    /// <summary>
+    /// 确保前值大于后值
+    /// </summary>
+    public static int GreaterThan(int value, int minValue, string parameterName)
+    {
+        if (value <= minValue)
+        {
+            throw new ArgumentException($"{parameterName} must be greater than {minValue}!", parameterName);
+        }
+
+        return value;
+    }
+
+    /// <summary>
+    /// 确保前值大于等于后值
+    /// </summary>
+    public static int GreaterThanOrEqual(int value, int minValue, string parameterName)
+    {
+        if (value < minValue)
+        {
+            throw new ArgumentException($"{parameterName} must be greater than or equal to {minValue}!", parameterName);
+        }
+
+        return value;
+    }
 }
